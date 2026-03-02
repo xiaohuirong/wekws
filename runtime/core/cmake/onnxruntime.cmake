6f0417a4d95eae57f0f3fa25f2511bac32c96b68
hoream <hoream@qq.com>
2026-03-02 21:00:48 +0800

    fix: fix aarch64 onnxruntime SHA256

--- runtime/core/cmake/onnxruntime.cmake
+++ runtime/core/cmake/onnxruntime.cmake
@@ -1,7 +1,7 @@
 set(ONNX_VERSION "1.18.0")
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
   set(ONNX_URL "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-aarch64-${ONNX_VERSION}.tgz")
-  set(URL_HASH "SHA256=12d189f78235216b22591e1d0032b85e090f70f63b46955d5d34193566779e56")
+  set(URL_HASH "SHA256=c278ca7ce725d2b26cf1ec62c93affaec4145a9a3d7721fb5d1af5497136ca76")
 else()
   set(ONNX_URL "https://github.com/microsoft/onnxruntime/releases/download/v${ONNX_VERSION}/onnxruntime-linux-x64-${ONNX_VERSION}.tgz")
   set(URL_HASH "SHA256=fa4d11b3fa1b2bf1c3b2efa8f958634bc34edc95e351ac2a0408c6ad5c5504f0")
